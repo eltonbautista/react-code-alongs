57572ce25858aff6b3a55856e8441af53427fe9c
Elton Bautista <eltonbestaha@gmail.com>
2022-05-06 08:02:55 -0600

    commit

---
+++ code-along-lifting-state-up/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import BoilingVerdict from './components/BoilingVerdict'
+import Calculator from './components/Calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
